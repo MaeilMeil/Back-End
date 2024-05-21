bb4696718703c53f18616db87d4641abb9e8e19b
KMK_HM <kyun9151@gmail.com>
2024-05-21 16:06:12 +0900

    initial commit

--- src/main/java/com/maeilmail/MaeilMailApplication.java
+++ src/main/java/com/maeilmail/MaeilMailApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MaeilMailApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MaeilMailApplication.class, args);
     }
